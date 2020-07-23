bf79575b3a4a6f7b070a1657e92cab42b105f862
Josh <j0522469@amarillocollege.com>
2020-07-23 14:22:16 -0500

    first commit for testing purposes.

---
+++ journalApp/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace journalApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.searchTxt = new System.Windows.Forms.TextBox();
+            this.monthgroupBox = new System.Windows.Forms.GroupBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.hideBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.monthgroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(419, 87);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(138, 35);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // searchTxt
+            // 
+            this.searchTxt.Location = new System.Drawing.Point(145, 87);
+            this.searchTxt.Multiline = true;
+            this.searchTxt.Name = "searchTxt";
+            this.searchTxt.Size = new System.Drawing.Size(254, 35);
+            this.searchTxt.TabIndex = 1;
+            // 
+            // monthgroupBox
+            // 
+            this.monthgroupBox.Controls.Add(this.hideBtn);
+            this.monthgroupBox.Controls.Add(this.monthCalendar1);
+            this.monthgroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthgroupBox.Location = new System.Drawing.Point(134, 128);
+            this.monthgroupBox.Name = "monthgroupBox";
+            this.monthgroupBox.Size = new System.Drawing.Size(254, 297);
+            this.monthgroupBox.TabIndex = 2;
+            this.monthgroupBox.TabStop = false;
+            this.monthgroupBox.Text = "MONTH";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 34);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // hideBtn
+            // 
+            this.hideBtn.Location = new System.Drawing.Point(0, 208);
+            this.hideBtn.Name = "hideBtn";
+            this.hideBtn.Size = new System.Drawing.Size(134, 46);
+            this.hideBtn.TabIndex = 1;
+            this.hideBtn.Text = "HIDE";
+            this.hideBtn.UseVisualStyleBackColor = true;
+            this.hideBtn.Click += new System.EventHandler(this.hideBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.Red;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.Location = new System.Drawing.Point(405, 338);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(111, 46);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.Text = "EXIT";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(257, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "My Time Calendar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 537);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.monthgroupBox);
+            this.Controls.Add(this.searchTxt);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Calendar";
+            this.monthgroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox searchTxt;
+        private System.Windows.Forms.GroupBox monthgroupBox;
+        private System.Windows.Forms.Button hideBtn;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ journalApp/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace journalApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ControlBox = false;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            monthgroupBox.Show();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            searchTxt.Text = monthCalendar1.SelectionStart.ToString();
+        }
+
+        private void hideBtn_Click(object sender, EventArgs e)
+        {
+            monthgroupBox.Hide();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
